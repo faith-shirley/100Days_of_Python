df91062f42ecfe7ebef3223fef118dc230fc232d
Leon12-aaryson <158481415+faith-shirley@users.noreply.github.com>
2025-03-05 00:35:36 +0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,6 @@
+username = input("Enter username")
+password = input("Enter password")
+if username == "admin" and password == "1234":
+    print("Access granted")
+else:
+    print("Access denied")
