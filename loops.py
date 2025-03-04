6d619f23abbde3bd5bfb5f19181a27259d76fadf
Leon12-aaryson <158481415+faith-shirley@users.noreply.github.com>
2025-03-05 00:09:48 +0300

    first commit

---
+++ loops.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana","cherry","date","elderberry"]
+index = 0
+
+while index < len(fruits):
+    if fruits[index] == "date":
+        break
+    print(fruits[index])
+    index +=1
